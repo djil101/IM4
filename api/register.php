0c70a3ef0504b6ff108a60b334d6b3f496844bc8
djil101 <damiana.jil@gmail.com>
2026-05-09 15:35:39 +0200

    update auf hostpoint

--- api/register.php
+++ api/register.php
@@ -5,6 +5,7 @@ header('Content-Type: application/json');
 
 require_once '../system/config.php';
 
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $data = json_decode(file_get_contents("php://input"), true);
