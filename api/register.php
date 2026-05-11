bbd274d6d922915de9034b3e0551725d0f70880a
djil101 <damiana.jil@gmail.com>
2026-05-11 11:52:42 +0200

    Update register.php

--- api/register.php
+++ api/register.php
@@ -4,6 +4,10 @@ header('Content-Type: application/json');
 
 require_once '../system/config.php';
 
+$data = json_decode(file_get_contents("php://input"), true);
+echo json_encode(["debug" => $data]);
+exit;
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $data = json_decode(file_get_contents("php://input"), true);
